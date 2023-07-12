ed5e0a7b4b4fb80d14b12f693c10ef2998525538
Vimal2710 <139349324+Vimal2710@users.noreply.github.com>
2023-07-12 17:33:06 +0530

    Add files via upload

---
+++ basicpattern.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,i,j;
+	cout<<"enter number to print:";
+	cin>>a;
+	for(i=1;i<=a;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			cout<<j;
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
