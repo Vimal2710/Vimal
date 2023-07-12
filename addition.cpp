fa60a539d87c56c175c1a1e2a8c6d24582225d17
Vimal2710 <139349324+Vimal2710@users.noreply.github.com>
2023-07-12 17:36:44 +0530

    addition.cpp

--- addition.cpp
+++ addition.cpp
@@ -1,11 +1,11 @@
+// sum of two integers
 #include<iostream>
 using namespace std;
 int main()
 {
 	int a,b,sum;
-	cin>>a;
-	cin>>b;
-	sum=a+b;
+	cin>>a>>b; //get input from user
+	sum=a+b; //sum of two integers
 	cout<<sum;
 	return 0;
 }
